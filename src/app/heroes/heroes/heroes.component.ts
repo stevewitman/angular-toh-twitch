6c08a2a4c4b8620cb1cebf65cc32c502f6ad1f5e
stevewitman <stevewitman@gmail.com>
2018-02-07 18:58:50 -0700

    Change to SCSS, add styles for router-link-active

--- src/app/heroes/heroes/heroes.component.ts
+++ src/app/heroes/heroes/heroes.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'toh-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
 
